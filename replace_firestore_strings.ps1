830e762bc9a5604db37c695a1c84d5e43712d0ae
Bocatta Developer <dev@bocatta.com>
2026-05-17 11:06:51 -0600

    fix: gradle.properties corruption, CI JDK 11->17, cleanup junk files

--- replace_firestore_strings.ps1
+++
@@ -1,8 +0,0 @@
-$pattern = '"FirestoreCollections\.([A-Z_]+)"'
-Get-ChildItem -Path "C:/Users/ia/AndroidStudioProjects/bocatta/app/src/main/java" -Recurse -Filter *.kt | ForEach-Object {
-    $content = Get-Content $_.FullName -Raw
-    $newContent = [regex]::Replace($content, $pattern, 'FirestoreCollections.$1')
-    if ($newContent -ne $content) {
-        Set-Content -Path $_.FullName -Value $newContent -Encoding utf8
-    }
-}
